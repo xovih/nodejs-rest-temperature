157148cbac1d0b73b9305c06766175461970ebfb
Sofi Rahmatulloh <sora@Sofis-MacBook-Pro.local>
2021-11-01 21:01:36 +0700

    Device Model

--- models/Device.js
+++ models/Device.js
@@ -1,19 +1,17 @@
 const mongoose = require("mongoose")
 
-const DeviceSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      unique: true,
-      required: true,
-      minLength: 3,
-    },
-    location: {
-      type: String,
-      required: true,
-      minLength: 1,
-    }
-  }
-)
+const DeviceSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		unique: true,
+		required: true,
+		minLength: 3,
+	},
+	location: {
+		type: String,
+		required: true,
+		minLength: 2,
+	},
+})
 
 module.exports = mongoose.model("Device", DeviceSchema)
